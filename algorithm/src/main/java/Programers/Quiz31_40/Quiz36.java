5c044572101e6bd46bea193d14313a1cc5a600bf
nohwooyeol <dnfdufdl@naver.com>
2022-08-09 22:49:40 +0900

    알고리즘 문제 업데이트

--- algorithm/src/main/java/Programers/Quiz31_40/Quiz36.java
+++ algorithm/src/main/java/Programers/Quiz31_40/Quiz36.java
@@ -6,16 +6,17 @@ import java.util.HashSet;
 public class Quiz36 {
     public int solution(int[] nums) {
         int answer = 0;
-        HashSet<Integer> hashSet = new HashSet<>(); //HashSet은
-        for (Integer a : nums) {
-            hashSet.add(a);
+        HashSet<Integer> hashSet = new HashSet<>(); // HashSet은 중복을 없애는 컬렉션
+        for (Integer a : nums) { // a에 nums의 값을 초기화
+            hashSet.add(a); //hash에 담기는데 이때 a의 중복값은 배재
         }
-        if(hashSet.size()>=nums.length/2){
-            answer = nums.length/2;
+        if(hashSet.size()>=nums.length/2){ // 중복된 값을 뺀 헤쉬의 길이가 넘어온배열의길이를 나누기 2한 것보다 크면 트루
+                                            //제한조건이 num/2
+            answer = nums.length/2;        //중복된 값을 빼면 그수안에서 수를 정하면 되니 길이나누기2의 값이 최대값이 된다.
         }else {
-            answer = hashSet.size();
+            answer = hashSet.size(); // 중복된값을뺀 해쉬의 길이가 더 길면 그만큼이 최대값
         }
-        return answer;
+        return answer; //답
     }
 
     public static void main(String[] args) {
@@ -25,7 +26,7 @@ public class Quiz36 {
         for (Integer a : dap) {
             hashSet.add(a);
         }
-
+        System.out.println(hashSet);
         if(hashSet.size()>=dap.length/2){
             answer = dap.length/2;
         }else {
